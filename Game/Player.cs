c33b5ea2a6bae2bcd6548dd80761059f2cfb8ff1
Matthieu Chambrion <matthieu.chambrion@epita.fr>
2020-08-03 15:29:02 +0200

    joueur map

--- Game/Player.cs
+++ Game/Player.cs
@@ -6,19 +6,13 @@ public class Player : KinematicBody2D
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
-	
-	private Sprite Screamer()
-	{
-		return GetNode<Node2D>("Screamer").GetNode<Sprite>("Sprite");
-	}
-	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		
 	}
 	
-
 	private const float gravity = 400.0f;
 	private const int walkSpeed = 200;
 
@@ -44,12 +38,9 @@ public class Player : KinematicBody2D
 
 	private void _on_Signal_body_entered(object body)
 	{
-		if (body == this)
-		{
-			GD.Print("oui");
-		}
+		GD.Print("oaie");
 	}
-	
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
@@ -57,6 +48,5 @@ public class Player : KinematicBody2D
 		var collision = MoveAndCollide(velocity * delta);
 		if (collision != null)
 			velocity = velocity.Slide(collision.Normal);
-		
 	}
 }
