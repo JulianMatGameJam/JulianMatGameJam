ee6f19f8519663fcc65c7a3fda3839f87a1ee362
Matthieu Chambrion <matthieu.chambrion@epita.fr>
2020-08-05 18:03:04 +0200

    bg

---
+++ Game v4 (map+wagon ^^)/Box.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Box : RigidBody2D
+{
+	
+	public override void _Ready()
+	{
+		
+	}
+	
+	
+	
+
+
+
+  public override void _Process(float delta)
+  {
+	  
+  }
+}
+
+
+
+
+
+
+
+
